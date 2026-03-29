d7fd4094d55406a9c389a8bc225bc8fb1ab27280
karthikmanepalli08-commits <karthikmanepalli08@gmail.com>
2026-03-29 23:32:08 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Upload from "./pages/Upload";
+import Results from "./pages/Results";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/upload" element={<Upload />} />
+      <Route path="/results" element={<Results />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+    </Routes>
+  );
+}
+
+export default App;
